40fde5460d235593f21d217d6e1a2d0ddae31186
Luis <79459748+luisalcab@users.noreply.github.com>
2022-11-01 07:57:30 -0600

    Challenge1 first changes

---
+++ Challenge1/line.cpp
@@ -0,0 +1,27 @@
+// Lines drawing algorithm
+/*
+Autor: 
+Luis Alberto Alcántara Cabrales A01634185
+
+Descripción:
+Dado dos puntos en el formato (x,y) se dibuja 
+una línea entre ellos utilizando el algoritmo 
+de Bresenham.
+
+Fecha: 1/11/2022
+*/
+
+#include <iostream>
+using namespace std;
+
+int main (){
+    cout << "Lines drawing algorithm" << endl;
+
+    // Input points
+    int x1 = 0;
+    int y1 = 0;
+    int x2 = 0;
+    int y2 = 0;
+
+    return 0;
+}
